bca2d510b13998110f9991a5aa4eef879d7b2cbd
Dia1104 <sadiataskeen380@gmail.com>
2026-02-15 23:38:00 +0530

    port fix

--- server.js
+++ server.js
@@ -177,4 +177,6 @@ app.get("/comments/:postid", (req, res) => {
 });
 
 // ================= START SERVER =================
-app.listen(process.env.PORT || 3000)
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Server running"));
+
