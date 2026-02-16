51533c85e933394467da24029474f9fb467b2885
Dia1104 <sadiataskeen380@gmail.com>
2026-02-16 23:47:02 +0530

    database connect railway

--- db.js
+++ db.js
@@ -1,15 +1,22 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "dsu_forum"
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 db.connect(err => {
-  if (err) throw err;
-  console.log("MySQL Connected");
+  if (err) {
+    console.log("❌ DB connection failed:", err);
+  } else {
+    console.log("✅ MySQL Connected");
+  }
 });
 
 module.exports = db;
